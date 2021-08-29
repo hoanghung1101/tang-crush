932aff77881be33646624da1ea13abd1047257cd
hoanghung1101 <77979210+hoanghung1101@users.noreply.github.com>
2021-08-29 13:36:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘😘',
     messDesc: 'Nào hết dịch đi tơi nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/TinhQuaSau' //link mess của các bạn. VD: https://m.me/nam.nodemy
