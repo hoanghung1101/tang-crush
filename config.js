1ae134db65cc9ec3f6d62cafe709e1810f368355
hoanghung1101 <77979210+hoanghung1101@users.noreply.github.com>
2021-08-29 13:20:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Trang à!',
+    introTitle: 'hahahahahahahahahha',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
